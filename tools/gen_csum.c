72eb3a2d6dc79ede5143e7dd92c42380a4cf490f
Owen <owen@easytarget.org>
2019-09-08 23:22:00 +0200

    Tools

---
+++ tools/gen_csum.c
@@ -0,0 +1,22 @@
+// Generate checksums for a few commands as needed
+// Owen, 20190908
+// lifted from: https://duet3d.dozuki.com/Wiki/Gcode#Section_Checking
+
+#include <stdio.h>
+
+void csum(char cmd[56])
+{
+  int cs = 0;
+  for(int i = 0; cmd[i] != '*' && cmd[i] != NULL; i++)
+   cs = cs ^ cmd[i];
+  cs &= 0xff;  // Defensive programming...
+  printf("%s*%u\n",cmd,cs);
+}
+
+void main()
+{
+  csum("M408 S0");
+  csum("M24");
+  csum("M25");
+}
+
